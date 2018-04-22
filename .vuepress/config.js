81c4f7cab2ee3eba559df03015f7cdb436600c3e
chengluyu <chengluyu@live.cn>
2018-04-22 22:49:23 +0800

    fix: upload source instead of html

--- .vuepress/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  title: 'SDU Lambda',
-  description: 'Shandong University special interest group of functional programming languages',
-  themeConfig: {
-    nav: [
-      { text: 'GitHub', link: 'https://github.com/SDULambda' },
-    ]
-  }
-}
