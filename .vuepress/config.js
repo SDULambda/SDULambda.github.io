a397ca1216a671850834c09ad7b6755e545b4813
chengluyu <chengluyu@live.cn>
2018-04-22 22:37:26 +0800

    feat: homepage

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'SDU Lambda',
+  description: 'Shandong University special interest group of functional programming languages',
+  themeConfig: {
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/SDULambda' },
+    ]
+  }
+}
